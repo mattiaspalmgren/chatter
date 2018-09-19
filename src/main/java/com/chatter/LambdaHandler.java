c7084219a8b365f831551137ec280ac7111a52fb
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2018-09-19 08:17:59 +0200

    Add lambda handler and cloudformation setup

---
+++ src/main/java/com/chatter/LambdaHandler.java
@@ -0,0 +1,34 @@
+package com.chatter;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class LambdaHandler implements RequestStreamHandler {
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Chatter.class);
+            handler.activateSpringProfiles("lambda");
+        } catch (ContainerInitializationException e) {
+            // Re-throw the exception to force another cold start
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+        // Just in case it wasn't closed by the mapper
+        outputStream.close();
+    }
+}
+
